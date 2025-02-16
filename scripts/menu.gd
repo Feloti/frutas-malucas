fc83c92d0d00e22f629718d2c6c48d27c28718c4
Osorio <osorior2021@gmail.com>
2025-02-16 14:46:23 -0300

    Adicionado botao de mute e cena de opções
    Agora tem como mutar e desmutar o jogo

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	MusicGame.play_music_level()
 
 func _on_config_button_pressed() -> void:
-	pass
+	get_tree().change_scene_to_file("res://scenes/Options.tscn")
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/selection.tscn")
